38d017cdf78591922cdcb2e3afb0236940db3b3e
Alejandro Loayza <alexloayza13@gmail.com>
2024-02-27 11:14:42 +0100

    renderizar listado de Todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,15 +1,28 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw'; //* ?raw para importar en crudo
+import { renderTodos } from './use-cases/render.todos';
+
+const ElementIDs = {
+    TodoList: '.todo-list'
+}
+
 /**
  * 
  * @param {String} elementId 
  */
 export const App = (elementId) => {
+
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos( ElementIDs.TodoList, todos );
+    }
     
     //* función anónima autoinvocada; Cuando la función se App() se llama
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 
 }
