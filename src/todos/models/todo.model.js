7349e50f46b0b876b61b7a8c4c2ef8e365c73da3
Alejandro Loayza <alexloayza13@gmail.com>
2024-02-27 11:38:59 +0100

    validaciones y optimizaciones

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -3,11 +3,11 @@ import {v4 as uuid} from 'uuid';
 export class Todo {
     /**
      * 
-     * @param {String} decription 
+     * @param {String} description 
      */
     constructor( decription ) {
         this.id = uuid();
-        this.decription = decription;
+        this.description = decription;
         this.done = false;
         this.createdAt = new Date();
     }
